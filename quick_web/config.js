3fd2c4eed3c8332864b4254ca21ce75c17f68531
Mr. Spacks <94480894+MrSpacks@users.noreply.github.com>
2025-09-25 21:20:02 +0200

    downgraded API

--- quick_web/config.js
+++ quick_web/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "/";
-// export const API_BASE_URL = "http://13.61.13.177:8000/";
+// export const API_BASE_URL = "/";
+export const API_BASE_URL = "http://13.61.13.177:8000/";
 // export const API_BASE_URL = "http://localhost:8000/api/"

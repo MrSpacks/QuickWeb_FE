9ce7fe4d35a726c81113cc58962a60d46369d941
Mr. Spacks <94480894+MrSpacks@users.noreply.github.com>
2025-09-27 19:27:59 +0200

    1

--- quick_web/config.js
+++ quick_web/config.js
@@ -1,3 +1,5 @@
 // export const API_BASE_URL = "/";
-export const API_BASE_URL = "http://13.61.13.177:8000/";
+export const API_BASE_URL = "http://13.62.115.181:8000/";
+
 // export const API_BASE_URL = "http://localhost:8000/api/"
+// export const API_BASE_URL = "http://api.webcardss.click/";
